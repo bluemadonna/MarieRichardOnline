2d120a31a382465d6438b31f749716123c417ad3
bluemadonna <83340817+bluemadonna@users.noreply.github.com>
2026-08-13 18:07:29 +0500

    Add files via upload

---
+++ js/cursor.js
@@ -0,0 +1,6 @@
+import { fairyDustCursor } from "https://unpkg.com/cursor-effects@latest/dist/esm.js";
+
+new fairyDustCursor({
+  colors: ["#ff2d95", "#fff34d", "#ff8fc7", "#c2185b"],
+  fairySymbol: "★"
+});
